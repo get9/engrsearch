fe55954bcfd8f60a66423b5581c7c167d08b9480
Sean Karlage <sean.karlage@gmail.com>
2015-03-22 20:24:22 -0400

    Added capability to store compressed info from tags

--- engrscrape/items.py
+++ engrscrape/items.py
@@ -5,6 +5,7 @@ class URLItem(Item):
     url = Field()
     xhash = Field()
     outlinks = Field()
+    compressed_data = Field()
 
     # Override this so the item doesn't get printed in the log
     def __repr__(self):
